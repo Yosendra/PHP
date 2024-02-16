0ac4e8ea900ad59f837ea13163ad6d0d0e3dcfeb
Yosendra <yosendra.evri@gmail.com>
2024-02-17 03:35:52 +0800

    PHP Dasar - 28. Function

---
+++ 01. PHP Dasar/28. Function/Program.php
@@ -0,0 +1,20 @@
+<?php
+/* 
+
+Function
+Function adalah blok kode program yang akan berjalan saat kita panggil
+
+function nama_function(){       // mendefinisikan function
+    statement function
+}
+
+nama_function();                // memanggil function
+
+*/
+
+function sayHello() {
+    echo "Hello Function" . PHP_EOL;
+}
+
+sayHello();
+sayHello();
