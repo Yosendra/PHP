fbc98f007b374cbafd43a4000f1204b0d41ea258
Yosendra <yosendra.evri@gmail.com>
2024-02-17 07:15:28 +0800

    PHP Dasar - 36. Komentar

---
+++ 01. PHP Dasar/36. Komentar/Program.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+
+Komentar adalah kode program yang akan dihiraukan saat kode program dijalankan.
+Biasanya komentar digunakan untuk dokumentasi.
+
+*/
+
+/*
+ Komentar multi baris
+ Lebih dari satu baris
+*/
+
+// Komentar satu baris
