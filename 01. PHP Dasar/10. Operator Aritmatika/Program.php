d39d420692953b0dbcee44dd6dfe4630157cc8c6
Yosendra <yosendra.evri@gmail.com>
2024-02-16 23:53:07 +0800

    PHP Dasar - 10. Operator Aritmatika

---
+++ 01. PHP Dasar/10. Operator Aritmatika/Program.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+
+Operator Aritmatika:
+• +$a           Positif
+• -$a           Negatif
+• $a + $b       Penambahan
+• $a - $b       Pengurangan
+• $a * $b       Perkalian
+• $a / $b       Pembagian
+• $a % $b       Sisa bagi
+• $a ** $b      Pangkat
+
+*/
+
+$a = 10;
+$b = 5;
+
+echo (+$a) . "\n";
+echo (-$a) . "\n";
+echo ($a + $b) . "\n";
+echo ($a - $b) . "\n";
+echo ($a * $b) . "\n";
+echo ($a / $b) . "\n";
+echo ($a % $b) . "\n";
+echo ($a ** $b) . "\n";
