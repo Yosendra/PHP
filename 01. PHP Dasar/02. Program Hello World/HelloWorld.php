c43f704afd2df480cc4c3a69e87f8e79e07c19f8
Yosendra <yosendra.evri@gmail.com>
2024-02-16 21:32:16 +0800

    PHP Dasar - 02. Program Hello World

---
+++ 01. PHP Dasar/02. Program Hello World/HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+
+• File kode program PHP diakhiri dengen extension .php
+• Di awal kode program PHP, wajib menambahkan <?php
+• Untuk menampilkan tulisan di PHP, gunakan perintah "echo"
+
+Untuk running file php lewat CMD, masuk terlebih dahulu ke direktori di CMD.
+Kemudian ketik perintah "php [nama_file_php]". Contoh: php HelloWorld.php.
+Nama file case-sensitive.
+
+*/
+
+echo "Hello World";
