b68c26cafa0ed2f97368a1cb92042ffe3bafaf14
Yosendra <yosendra.evri@gmail.com>
2024-02-17 02:57:48 +0800

    PHP Dasar - 22. For Loop

---
+++ 01. PHP Dasar/22. For Loop/Program.php
@@ -0,0 +1,13 @@
+<?php
+/* 
+
+For loop statement
+for (init statement; condition; post statement){
+    // loop statement
+}
+
+*/
+
+for ($counter = 0; $counter < 10; $counter++) { 
+    echo "Looping 10 times" . PHP_EOL;
+}
