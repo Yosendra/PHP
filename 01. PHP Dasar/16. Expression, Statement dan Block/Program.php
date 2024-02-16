99649234490a4c167db9be79026d3ae96846d53c
Yosendra <yosendra.evri@gmail.com>
2024-02-17 01:27:17 +0800

    PHP Dasar - 16. Expression, Statement dan Block

---
+++ 01. PHP Dasar/16. Expression, Statement dan Block/Program.php
@@ -0,0 +1,22 @@
+<?php
+/* 
+
+Expression, Statement, Block
+
+Expression adalah apapun yang memiliki nilai
+Contoh: $a = 5; 5 adalah expression
+
+Statement adalah kalimat lengkap dalam bahasa.
+Sebuah statement berisikan execution komplit dan biasanya diakhir dengan titik koma (;).
+$name = "John Doe";     // statement 1
+echo $name;             // statement 2
+
+Block adalah kumpulan statement yang terdiri dari nol atau lebih statement.
+Block diawali dan diakhiri dengan kurung kurawal ({})
+{                       // Block
+    echo "Start";
+    echo "Hello World"
+    echo "End";
+}
+
+*/
