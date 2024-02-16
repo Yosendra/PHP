4fb769c6ac2d1b8693a17e66870acdcd5618a993
Yosendra <yosendra.evri@gmail.com>
2024-02-16 22:20:32 +0800

    PHP Dasar - 06. Variable

---
+++ 01. PHP Dasar/06. Variable/Program.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+
+Variable adalah tempat untuk menyimpan data sehingga bisa digunakan kembali di kode program selanjutnya
+Di PHP, variable bisa menampung berbagai jenis tipe data dan bisa berubah-ubah
+Untuk membuat variable, gunakan tanda $ diikuti dengan nama variablenya
+Penamaan variable tidak boleh mengandung spasi
+
+*/
+
+$name = "John";
+$age = 21;
+
+echo "Name : $name";
+echo "\n";
+echo "Age : $age";
