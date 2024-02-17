43d132bee1f93df81fd467a33f29e66487503d23
Yosendra <yosendra.evri@gmail.com>
2024-02-18 00:35:29 +0800

    Studi Kasus PHP Dasar - 04. Menentukan View

---
+++ 02. Studi Kasus PHP Dasar - To Do List/04. Menentukan View/App.php
@@ -0,0 +1,21 @@
+<?php
+/*
+
+View adalah tampilan.
+
+Look at: 
+• ViewAddTodoList.php
+• ViewRemoveTodoList.php
+• ViewShowTodoList.php
+
+*/
+
+require_once "Model/TodoList.php";
+require_once "BusinessLogic/AddTodoList.php";
+require_once "BusinessLogic/RemoveTodoList.php";
+require_once "BusinessLogic/ShowTodoList.php";
+require_once "View/ViewAddTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
+require_once "View/ViewShowTodoList.php";
+
+echo "Aplikasi TodoList" . PHP_EOL;
