a11a1ff26b891d36b65bc028b75b0f7eb3ed1839
Yosendra <yosendra.evri@gmail.com>
2024-02-18 19:16:47 +0800

    PHP OOP - 02. Class

---
+++ 03. PHP OOP/02. Class/Program.php
@@ -0,0 +1,17 @@
+<?php
+/*
+
+Gunakan keyword "class" untuk membuat class.
+Penamaan class biasanya menggunakan format CamelCase.
+
+*/
+
+class Person        // ini class
+{
+
+}
+
+class Car           // ini juga class
+{
+
+}
