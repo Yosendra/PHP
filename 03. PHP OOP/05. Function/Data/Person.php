1f82d34c43f60fad06c7c1b8afa0683e22657dd4
Yosendra <yosendra.evri@gmail.com>
2024-02-18 20:04:42 +0800

    PHP OOP - 05. Function

---
+++ 03. PHP OOP/05. Function/Data/Person.php
@@ -0,0 +1,13 @@
+<?php
+
+class Person
+{
+    var string $name;
+    var string $address;
+    var string $country;
+
+    function sayHello(string $name)         // deklarasikan method
+    {
+        echo "Hello $name" . PHP_EOL;
+    }
+}
---
+++ 03. PHP OOP/05. Function/Program.php
@@ -0,0 +1,20 @@
+<?php
+/*
+
+Function (Method)
+Selain menambahkan properties, kita bisa juga menambahkan method ke object
+dengan cara mendeklarasikan function di dalam blok class.
+Untuk mengakses method dari object, gunakan tanda "->" diikuti nama functionnya
+sama seperti mengakses property.
+
+Look at:
+• Person.php
+• Program.php
+
+*/
+
+require_once __DIR__ . "/Data/Person.php";
+
+
+$person = new Person();
+$person->sayHello("Alex");      // mengakses method dari object
