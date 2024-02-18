06ec7da4b1fd815eca3fb968bff13fa7cfc80721
Yosendra <yosendra.evri@gmail.com>
2024-02-18 23:19:01 +0800

    PHP OOP - 07. Constant

---
+++ 03. PHP OOP/07. Constant/Data/Person.php
@@ -0,0 +1,18 @@
+<?php
+
+class Person
+{
+    const AUTHOR = "John Doe";          // const di dalam class
+
+    var string $name;
+    var string $address;
+    var string $country;
+
+    function sayHello(?string $name)
+    {
+        if (is_null($name))
+            echo "Hi, my name is {$this->name}" . PHP_EOL;
+        else
+            echo "Hello $name, my name is {$this->name}" . PHP_EOL;
+    }
+}
---
+++ 03. PHP OOP/07. Constant/Program.php
@@ -0,0 +1,24 @@
+<?php
+/*
+
+Constant
+Properties di class nilainya bisa berubah (mutable).
+Kita dapat membuatnya menjadi constant (tidak dpt berubah, immutable).
+Sebelumnya kita depat mendefinisikan constant dengan function "define()"
+Sejak PHP 7.4 kita bisa menggunakan keyword "const" untuk membuat constant.
+Pendefinisiannya mirip seperti variable namun kali ini tidak menggunakan karakter $.
+
+Cara mengakses constant yang ada di dalam class dengan menggunakan tanda titik dua (:)
+sebanyak dua kali di belakang nama class, kemudian diikuti nama constantnya
+
+Look at:
+• Constant.php
+• Person.php
+• Program.php
+
+*/
+
+require_once __DIR__ . "/Data/Person.php";
+
+
+echo Person::AUTHOR . PHP_EOL;          // mengakses constant di dalam class
