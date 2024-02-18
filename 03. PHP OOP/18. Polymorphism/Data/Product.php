c733100d5e626cbbd8b21969ba117cd556b550ea
Yosendra <yosendra.evri@gmail.com>
2024-02-19 04:00:45 +0800

    PHP OOP - 18. Polymorphism

---
+++ 03. PHP OOP/18. Polymorphism/Data/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product
+{
+    private string $name;
+    private int $price;
+
+    public function __construct(string $name, int $price) 
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
---
+++ 03. PHP OOP/18. Polymorphism/Program.php
@@ -0,0 +1,30 @@
+<?php
+/*
+
+Polymorphism
+Polymorphism berarti banyak bentuk.
+Dalam OOP, Polymorphism adalah kemampuan sebuah object berubah menjadi bentuk lain.
+Polymorphism erat hubungannya dengan inheritance.
+
+Look at:
+• Programmer.php
+
+*/
+
+require_once __DIR__ . "/Data/Programmer.php";
+
+$company1 = new Company();
+$company2 = new Company();
+$company3 = new Company();
+
+# property programmer dari company bertipe "Programmer"
+# namun kita bisa mengisinya dengan object dari child kelas milik Programmer
+# yaitu 'BackendProgrammer' atau 'FrontendProgrammer'
+$company1->programmer = new Programmer("John");
+$company2->programmer = new BackendProgrammer("Alex");
+$company3->programmer = new FrontendProgrammer("Suzan");
+
+# sayHello mampu menerima objek dari Programmer dan turunannya sebagai argumen
+sayHello(new Programmer("John"));
+sayHello(new BackendProgrammer("Alex"));
+sayHello(new FrontendProgrammer("Suzan"));
