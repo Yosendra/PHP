2d583f713e5c1316e583096546994aefc7e9a148
Yosendra <yosendra.evri@gmail.com>
2024-02-19 00:39:23 +0800

    PHP OOP - 11. Inheritance

---
+++ 03. PHP OOP/11. Inheritance/Data/ViceManager.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . "/Manager.php";
+
+class ViceManager extends Manager
+{
+    // semua property dan method yang di dalam kelas Manager
+    // akan dimiliki juga oleh kelas ViceManager tanpa perlu mendeklarasikannya
+    // kembali
+}
---
+++ 03. PHP OOP/11. Inheritance/Program.php
@@ -0,0 +1,27 @@
+<?php
+/*
+
+Inheritance
+Inheritance atau pewarisan adalah kemampuan untuk menurunkan sebuah class ke class lain.
+Relasi antar dua class adalah Parent-Child.
+Child class hanya bisa punya satu Parent class, namun satu Parent class bisa punya banyak Child class.
+Saat sebuah class diturunkan, maka semua properties dan method yang ada di dalam Parent class,
+secara otomatis akan dimiliki juga oleh Child class.
+Gunakan keyword 'extends' untuk melakukan inheritance. 
+
+Look at:
+• Manager.php
+• ViceManager.php
+
+*/
+
+require_once __DIR__ . "/Data/Manager.php";
+require_once __DIR__ . "/Data/ViceManager.php";
+
+$manager = new Manager();
+$manager->name = "Manager";
+$manager->sayHello("ViceManager");
+
+$manager = new ViceManager();
+$manager->name = "ViceManager";     // property dari kelas Manager
+$manager->sayHello("Manager");      // method dari kelas Manager
