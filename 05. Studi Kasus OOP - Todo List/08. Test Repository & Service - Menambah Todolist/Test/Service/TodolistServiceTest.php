65d572b2d501cacb1d5efc63f65eb0b997bf9b71
Yosendra <yosendra.evri@gmail.com>
2024-02-25 20:31:38 +0800

    Studi Kasus OOP - 08. Test Repository & Service: Menambah Todolist

---
+++ 05. Studi Kasus OOP - Todo List/08. Test Repository & Service - Menambah Todolist/Test/Service/TodolistServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Test\Service;
+
+require_once __DIR__ . "/../../Service/TodolistService.php";
+require_once __DIR__ . "/../../Repository/TodolistRepository.php";
+require_once __DIR__ . "/../../Entity/Todolist.php";
+
+use Repository\TodolistRepository;
+use Service\TodolistService;
+use Entity\Todolist;
+
+function testShowTodolist()
+{
+    $todolistRepository = new TodolistRepository();
+    $todolistRepository->todolist[1] = new Todolist("Belajar PHP Dasar");
+    $todolistRepository->todolist[2] = new Todolist("Belajar PHP OOP");
+    $todolistRepository->todolist[3] = new Todolist("Belajar PHP Database");
+
+    $todolistService = new TodolistService($todolistRepository);
+
+    $todolistService->showTodolist();
+}
+
+function testAddTodolist()
+{
+    $todolistRepository = new TodolistRepository();
+    $todolistService = new TodolistService($todolistRepository);
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+    $todolistService->addTodolist("Belajar PHP Database");
+    
+    $todolistService->showTodolist();
+}
+
+// testShowTodolist();
+testAddTodolist();
