2ceb4308bb74f4a23e2bb4e36c8a6ef3fa1c5ced
Yosendra <yosendra.evri@gmail.com>
2024-02-27 01:37:39 +0800

    Studi Kasus OOP - 14. Test View: Menambah Todolist

---
+++ 05. Studi Kasus OOP - Todo List/14. Test View - Menambah Todolist/View/TodolistView.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace View;
+
+require_once __DIR__ . "/../Service/TodolistService.php";
+require_once __DIR__ . "/../Helper/InputHelper.php";
+
+use Helper\InputHelper;
+use Service\ITodolistService;
+
+class TodolistView
+{
+    private ITodolistService $todolistService;
+
+    public function __construct(ITodolistService $todolistService) 
+    {
+        $this->todolistService = $todolistService;
+    }
+
+    function showTodolist(): void
+    {
+        while (true):
+            $this->todolistService->showTodolist();
+            
+            echo "MENU" . PHP_EOL;
+            echo "1. Tambah Todo" . PHP_EOL;
+            echo "2. Hapus Todo" . PHP_EOL;
+            echo "x. Keluar" . PHP_EOL;
+    
+            $pilihan = trim(InputHelper::input("Pilih"));
+            if ($pilihan == "1")
+                $this->addTodolist();
+            elseif ($pilihan == "2")
+                $this->removeTodolist();
+            elseif ($pilihan == "x")
+                break;
+            else
+                echo "Pilihan tidak dimengerti." . PHP_EOL;
+
+            echo PHP_EOL;
+        endwhile;
+    
+        echo "Sampai Jumpa Lagi" . PHP_EOL;
+    }
+
+    function addTodolist(): void
+    {
+        echo "MENAMBAH TODO" . PHP_EOL;
+
+        $todo = InputHelper::input("Todo (x untuk batal)");
+        if ($todo == "x")
+            echo "Batal menambah TODO" . PHP_EOL;
+        else
+            $this->todolistService->addTodolist($todo);
+    }
+
+    function removeTodolist(): void
+    {
+
+    }
+}
