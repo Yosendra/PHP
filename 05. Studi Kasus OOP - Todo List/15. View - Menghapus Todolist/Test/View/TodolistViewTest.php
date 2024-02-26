b834bfb5a14aa3e8d1fa381e7c9cd683c700db9d
Yosendra <yosendra.evri@gmail.com>
2024-02-27 01:44:23 +0800

    Studi Kasus OOP - 15. View: Menghapus Todolist

---
+++ 05. Studi Kasus OOP - Todo List/15. View - Menghapus Todolist/Test/View/TodolistViewTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Test\View;
+
+require_once __DIR__ . "/../../View/TodolistView.php";
+require_once __DIR__ . "/../../Service/TodolistService.php";
+require_once __DIR__ . "/../../Repository/TodolistRepository.php";
+
+use Repository\TodolistRepository;
+use Service\TodolistService;
+use View\TodolistView;
+
+function testViewShowTodoList(): void
+{
+    $todolistRepository = new TodolistRepository();
+    $todolistService = new TodolistService($todolistRepository);
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+    $todolistService->addTodolist("Belajar PHP Database");
+
+    $todolistView = new TodolistView($todolistService);
+    $todolistView->showTodolist();
+}
+
+function testViewAddTodoList(): void
+{
+    $todolistRepository = new TodolistRepository();
+    $todolistService = new TodolistService($todolistRepository);
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+    $todolistService->addTodolist("Belajar PHP Database");
+
+    $todolistService->showTodolist();
+
+    $todolistView = new TodolistView($todolistService);
+    $todolistView->addTodolist();
+
+    $todolistService->showTodolist();
+}
+
+// testViewShowTodoList();
+testViewAddTodoList();
