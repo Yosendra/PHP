21551b2eb3a1aeacd2f8529963be5884464bee10
Yosendra <yosendra.evri@gmail.com>
2024-02-27 00:41:46 +0800

    Studi Kasus OOP - 10. Test Repository dan Service: Menghapus Todolist

---
+++ 05. Studi Kasus OOP - Todo List/10. Test Repository dan Service - Menghapus Todolist/Repository/TodolistRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Repository;
+
+use Entity\Todolist;
+
+interface ITodolistRepository
+{
+    function save(Todolist $todolist): void;
+    function remove(int $number): bool;
+    function findAll(): array;
+}
+
+class TodolistRepository implements ITodolistRepository
+{
+    public array $todolist = array();
+
+    function save(Todolist $todolist): void
+    {
+        $number = sizeof($this->todolist) + 1;
+        $this->todolist[$number] = $todolist;
+    }
+
+    function remove(int $number): bool
+    {
+        $lastIndex = sizeof($this->todolist);
+        if ($number > $lastIndex)
+            return false;
+
+        for ($i = $number; $i < $lastIndex; $i++)
+            $this->todolist[$i] = $this->todolist[$i + 1];
+
+        unset($this->todolist[$lastIndex]);
+
+        return true;
+    }
+
+    function findAll(): array
+    {
+        return $this->todolist;
+    }
+}
