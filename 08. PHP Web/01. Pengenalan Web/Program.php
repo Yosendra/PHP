68a02bb669e9ee8768c87b883f4fa584e413f75e
Yosendra <yosendra.evri@gmail.com>
2024-02-27 10:16:17 +0800

    PHP Web - 01. Pengenalan Web

---
+++ 08. PHP Web/01. Pengenalan Web/Program.php
@@ -0,0 +1,44 @@
+<?php
+/*
+
+Web != Internet
+
+Internet adalah mekanisme komunikasi antar komputer.
+Di era awal internet, komunikasi antar komputer membutuhkan jaringan kabel telpon.
+Sekarang dengan menjamurnya jaringan wifi dan sejenisnya, komunikasi antar komputer menjadi lebih cepat dan mudah.
+
+Web merupakan kumpulan informasi yang tersedia dalam sebuah komputer yang terkoneksi secara terus menerus melalui internet.
+Web bisa berisi informasi dalam bentuk apapun seperti teks, gambar, audio, video, dan lainnya.
+Web berjalan di aplikasi yang bernama web server yaitu aplikasi yang digunakan untuk menyimpan dan menyampaikan isi informasi web.
+
+                                         |                              |
+Komputer (client) -----> Internet -----> | Komputer -> WebServer -> Web |
+                                         |                              |
+
+Web Host
+Pemilik web biasanya tidak menjalankan aplikasi web server di komputer pribadi.
+Mereka akan menyewa komputer di tempat penyedia data center (kumpulan komputer yang terjamin keandalan dan kecepatan koneksi internetnya).
+Pihak penyedia komputer untuk web server biasa disebut web host.
+
+Domain
+Saat komputer terhubung ke internet, biasanya dia memiliki alamat.
+Alamat ini bernama ip address. Formatnya misal 172.217.194.94
+Karena alamat ip address sangat menyulitkan untuk diingat.
+Maka ditentukanlah yang namanya domain.
+Domain adalah alamat yang digunakan sebagai alias ke ip address.
+Misal seperti google.co.id, tokopedia.com, dan lainnya.
+Dengan domain, kita akan mudah mengingat dibanding mengingat ip address.
+Namun saat kita menggunakan nama domain, sebenarnya komputer tetap mengakses menggunakan alamat ip address. 
+Melalui Domain Server didapatkan ip address berdasarkan domain address yang diminta, kemudian diteruskan ke alamat ip address dituju.
+
+Web Browser
+Jika web server adalah aplikasi yang digunakan untuk menyimpan informasi web.
+Web browser adalah aplikasi yang digunakan untuk mengakses web melalui internet.
+Kita bisa saja mengakses web secara langsung tanpa bantuan web browser, namun web server hanya akan memberikan informasi bahasa mesin
+seperti HTML, Javascript, CSS, Gambar, Video, dan lainnya.
+Dengan menggunakan web browser, semua bahasa mesin tersebut bisa ditampilkan secara visual sehingga kita bisa menyerap informasinya
+dengan lebih mudah.
+
+
+
+*/
