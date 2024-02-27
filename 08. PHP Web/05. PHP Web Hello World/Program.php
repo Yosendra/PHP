21f9ff97b3d3e780efff61f2a06d3602712313fa
Yosendra <yosendra.evri@gmail.com>
2024-02-27 19:24:20 +0800

    PHP Web - 05. PHP Web Hello World

---
+++ 08. PHP Web/05. PHP Web Hello World/Program.php
@@ -0,0 +1,14 @@
+<?php
+/*
+
+PHP Web Hello World
+Buat file hello_world.php dengan perintah php di dalamnya.
+Setelah itu jalankan PHP Development Server.
+Untuk mengakses file hello_world.php yang sudah dibuat, gunakan web browser dengan url: http://localhost:8080/nama_file.php
+Sesuaikan nama_file.php dengan nama file yang dibuat. Sehingga kini menjadi: http://localhost:8080/hello_world.php
+
+Best Practice URL di PHP
+Gunakan lowercase, misal /product.php, /login.php
+Jangan gunakan spasi, misal /hello-world.php, /forgot-password.php
+
+*/
