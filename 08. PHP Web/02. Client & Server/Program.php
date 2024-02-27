460220ddca9d943306563a5f7198cca787616788
Yosendra <yosendra.evri@gmail.com>
2024-02-27 18:22:08 +0800

    PHP Web - 02. Client & Server

---
+++ 08. PHP Web/02. Client & Server/Program.php
@@ -0,0 +1,27 @@
+<?php
+/*
+
+Client & Server
+Web adalah aplikasi berbasis Client & Service.
+Client & Server merupakan konsep arsitektur aplikasi yang menghubungkan dua pihak, sistem client & sistem server.
+Sistem client & sistem server saling berkomunikasi melalui jaringan komputer, internet, atau bisa komputer yang sama.
+
+Client -----> Server
+
+Tugas Client & Server
+Aplikasi client bertugas mengirim request ke server dan menerima response dari server.
+Aplikasi server bertugas menerima request dari client, memproses data, dan mengembalikan hasil proses data ke client.
+
+Keuntungan Client & Server
+Perubahan aplikasi bisa dilakukan dengan mudah di server tanpa harus membuat perubahan di client,
+apalagi jika client berada di lokasi yang sulit dijangkau.
+Dapat digunakan oleh banyak client pada saat yang bersamaan walaupun server tidak banyak.
+Dapat diakses dari server mana saja asal terhubung dalam satu jaringan dengan server.
+
+Contoh Client & Server
+Web adalah salah satu contoh arsitektur client-server.
+Aplikasi yang bertugas sebagai cleint adalah web browser (Chrome, Firefox, Opera, Edge, dll).
+Aplikasi yang bertugas sebagai server adalah web server, dimana di dalam web server terdapat
+kode program web kita.
+
+*/
