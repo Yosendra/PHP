592203781f1ba08627a56c05c2224e97f7412e6c
Yosendra <yosendra.evri@gmail.com>
2024-02-28 03:25:59 +0800

    PHP Web - 17. Session & Cookie

---
+++ 08. PHP Web/17. Session & Cookie/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+
+Session & Cookie
+Sebelumnya kita sudah belajar session di PHP. Sebenarnya sesion id di PHP di simpan di cookie.
+Ketika kita membuat session, PHP akan membuat cookie session id.
+Cookie session id tersebut digunakan sebagai id untuk file session di server PHP.
+Ketika web browser mengirim cookie berisi session id tersebut, PHP akan mengecek apakah nama
+file dengan session id tersebut ada atau tidak. Jika ada, otomatis sessionnya valid dan akan 
+diambil semua data session di dalam file tersebut.
+
+*/
+
