1ca1bbcec557b42c3295a9e832579271e08237a2
Yosendra <yosendra.evri@gmail.com>
2024-02-27 19:30:47 +0800

    PHP Web - 06. PHP Info

---
+++ 08. PHP Web/06. PHP Info/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+
+PHP Info
+Fitur ini bisa digunakan untuk mengecek versi PHP dan fitur apa saja yang tersedia di aplikasi PHP yang kita install.
+PHP Info bis dilihat menggunakan terminal dengan perintah 'php --info'
+Atau menggunakan PHP Web dengan memanggil fungsi 'phpinfo()'
+
+*/
+
+phpinfo();
