d7fba5d0fa16703c499f2453277eafb2aa8790f8
Yosendra <yosendra.evri@gmail.com>
2024-02-27 09:20:57 +0800

    Studi Kasus PHP MySQL - 06. Test Repository: Menghapus Todolist

---
+++ 07. Studi Kasus PHP MySQL - Todo List/06. Test Repository - Menghapus Todolist/Test/Service/TodolistServiceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Test\Service;
+
+require_once __DIR__ . "/../../Service/TodolistService.php";
+require_once __DIR__ . "/../../Repository/TodolistRepository.php";
+require_once __DIR__ . "/../../Entity/Todolist.php";
+require_once __DIR__ . "/../../Config/Database.php";
+
+use Config\Database;
+use Repository\TodolistRepository;
+use Service\TodolistService;
+use Entity\Todolist;
+
+function testShowTodolist()
+{
+    $todolistRepository = new TodolistRepository();
+    $todolistRepository->todolist[1] = new Todolist("Belajar PHP Dasar");
+    $todolistRepository->todolist[2] = new Todolist("Belajar PHP OOP");
+    $todolistRepository->todolist[3] = new Todolist("Belajar PHP Database");
+
+    $todolistService = new TodolistService($todolistRepository);
+
+    $todolistService->showTodolist();
+}
+
+function testAddTodolist()
+{
+    $connection = Database::getConnection();
+
+    $todolistRepository = new TodolistRepository($connection);
+    $todolistService = new TodolistService($todolistRepository);
+    $todolistService->addTodolist("Belajar PHP Dasar");
+    $todolistService->addTodolist("Belajar PHP OOP");
+    $todolistService->addTodolist("Belajar PHP Database");
+    
+    // $todolistService->showTodolist();
+}
+
+function testRemoveTodolist()
+{
+    $connection = Database::getConnection();
+    $todolistRepository = new TodolistRepository($connection);
+    $todolistService = new TodolistService($todolistRepository);
+    
+    echo $todolistService->removeTodolist(5) . PHP_EOL;
+    echo $todolistService->removeTodolist(4) . PHP_EOL;
+    echo $todolistService->removeTodolist(3) . PHP_EOL;
+    echo $todolistService->removeTodolist(2) . PHP_EOL;
+    echo $todolistService->removeTodolist(1) . PHP_EOL;
+}
+
+// testShowTodolist();
+// testAddTodolist();
+testRemoveTodolist();
