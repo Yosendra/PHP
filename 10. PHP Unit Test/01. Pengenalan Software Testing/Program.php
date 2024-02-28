9abf009c07ed8b844446349ffe7370bd4fdda2e6
Yosendra <yosendra.evri@gmail.com>
2024-02-29 07:26:31 +0800

    PHP Unit Test - 01. Pengenalan Software Testing

---
+++ 10. PHP Unit Test/01. Pengenalan Software Testing/Program.php
@@ -0,0 +1,18 @@
+<?php
+/*
+
+Pengenalan Software Testing
+Software testing adalah salah satu disiplin ilmu dalam software engineering.
+Tujuan utama dari software testing adalah memastikan kualitas kode dan aplikasi kita baik.
+Ilmu untuk software testing sendiri sangatlah luas, saat ini kita hanya fokus ke unit testing.
+
+Test Pyramid
+Unit (Unit Testing) -> Service (Integration Testing) -> UI (End-to-End Testing)
+
+Unit Test
+Unit test akan fokus menguji bagian kode program terkecil, biasanya menguji sebuah method.
+Unit test biasanya dibuat kecil dan cepat, oleh karena itu biasanya kadang kode unit test lebih 
+banyak dari kode program aslinya karena semua skenario pengujian akan dicoba di unit test.
+Unit test bisa digunakan sebagai cara untuk meningkatkan kulaitas kode program kita.
+
+*/
