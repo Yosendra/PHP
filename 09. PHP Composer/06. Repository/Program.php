d4626ef777f675d689704ad4ac6405b793e301ed
Yosendra <yosendra.evri@gmail.com>
2024-02-29 00:01:28 +0800

    PHP Composer - 06. Repository

---
+++ 09. PHP Composer/06. Repository/Program.php
@@ -0,0 +1,10 @@
+<?php
+/*
+
+Repository
+Repository merupakan tempat semua dependency disimpan.
+Secara default, composer menggunakan reposiroty Packagist: https://packagist.org/
+Kita juga bisaa menambahkan repository selai packagist jika mau: https://getcomposer.org/doc/05-repositories.md
+
+*/
+
