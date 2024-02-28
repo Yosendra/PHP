16c150751dc5f4ac7c90630b1ff9997747a8c676
Yosendra <yosendra.evri@gmail.com>
2024-02-28 20:42:49 +0800

    PHP Composer - 02. Menginstall Composer

---
+++ 09. PHP Composer/02. Menginstall Composer/Program.php
@@ -0,0 +1,21 @@
+<?php
+/*
+
+Menginstall Composer
+https://getcomposer.org/download/
+Download binary file composer, lalu masukan ke dalam path directory bin PHP, atau bisa gunakan direcotory terpisah,
+namun perlu kita menambahkan ke PATH lagi jika menggunakan directory terpisah.
+Setelah menginstall composer di PATH, kita bisa gunakan perintah berikut di terminal untuk mengecek versi composer.
+
+Download: composer.phar (Linux/Mac/Windows)
+Install: copy ke direktory "C:\xampp\php", rename composer.phar menjadi composer.bat
+Perintah: composer --version
+
+atau 
+
+Download: Composer-Setup.exe (Windows)
+Install: Install file exe. Composer terinstall di "C:\ProgramData\ComposerSetup\bin" 
+         Tambah lokasi direktory dari file binary php ke PATH.
+Perintah: jalan "composer --version" di terminal
+
+*/
